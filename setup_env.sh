6e9544ace42493b68b010a84b0a1f5af3c159761
Michael Dolce <mike.dolce8@gmail.com>
2025-03-19 15:44:00 -0500

    change the venv to the same one that is used for the training: py3.11-pipTF2.15.0. Defined a new plot path: WSU-Vertexer-Plots. Added option to find "Combined" flux from the helper function.

--- setup_env.sh
+++ setup_env.sh
@@ -5,11 +5,11 @@ echo WSUVTX is set: $WSUVTX
 echo '=========================='
 
 unset PYTHONPATH
-export PYTHONPATH=/homes/k948d562/virtual-envs/poetry1.7.0-test-py3.11-tf2.15/lib/python3.11/site-packages:$WSUVTX
+export PYTHONPATH=/homes/k948d562/virtual-envs/py3.11-pipTF2.15.0/lib/python3.11/site-packages:$WSUVTX
 echo PYTHONPATH is set: $PYTHONPATH
 echo '=========================='
 
-export LD_LIBRARY_PATH=/homes/k948d562/virtual-envs/poetry1.7.0-test-py3.11-tf2.15/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/homes/k948d562/virtual-envs/py3.11-pipTF2.15.0/lib:$LD_LIBRARY_PATH
 echo LD_LIBRARY_PATH is set: $LD_LIBRARY_PATH
 echo 
 
